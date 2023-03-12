54c04abe7ef0f6b52fdb72dd23e8a1541da89d18
Wolverine3993 <oscar@koopmanmail.com>
2023-03-12 21:45:26 +1100

    wertfwetg

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.XR;
 
 public class Movement : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class Movement : MonoBehaviour
     [Header("Movement")]
     [SerializeField] float movementSpeed;
     [SerializeField] float jumpHeight;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float deceleration;
     Rigidbody rb;
     [SerializeField] TMP_Text text;
     private void Start()
@@ -23,6 +26,10 @@ public class Movement : MonoBehaviour
     private void Update()
     {
         DoCameraShenanigans();
+        
+    }
+    private void FixedUpdate()
+    {
         DoMovement();
     }
     private void DoCameraShenanigans()
@@ -40,6 +47,18 @@ public class Movement : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         Vector3 move = transform.forward * z + transform.right * x;
         rb.AddForce(new Vector3(move.x, 0, move.z).normalized * movementSpeed);
-        text.text = $"Velocity X: {rb.velocity.x} \nVelocity Z: {rb.velocity.z}";
+        text.text = $"Velocity: {new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude}";
+        Vector3 targetThing = Vector3.ClampMagnitude(new Vector3(rb.velocity.x, 0, rb.velocity.z), maxSpeed);
+        if(new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude > maxSpeed)
+            rb.velocity = new Vector3(targetThing.x, rb.velocity.y, targetThing.z);
+        float xVal = 0;
+        float zVal = 0;
+        if (x == 0)
+            xVal = -0.5f;
+        if (z == 0)
+            zVal = -0.5f;
+        Debug.Log(z);
+        Vector3 good = rb.velocity.x * transform.right + rb.velocity.z * transform.forward;
+        rb.velocity = (new Vector3(good.x, rb.velocity.y, good.z));
     }
 }
