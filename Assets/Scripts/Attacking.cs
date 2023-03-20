c81b20bb430b663c0a248fe94bb278da257719b4
Wolverine3993 <oscar@koopmanmail.com>
2023-03-20 21:46:17 +1100

    rgberdhj

--- Assets/Scripts/Attacking.cs
+++ Assets/Scripts/Attacking.cs
@@ -6,12 +6,14 @@ public class Attacking : MonoBehaviour
 {
     [SerializeField] Animator anim;
     [SerializeField] LayerMask attackLayer;
+    [SerializeField] GameObject target;
     bool CanSwing = true;
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && CanSwing)
         {
             anim.SetBool("Swing", true);
+            Attack();
             CanSwing = false;
         }
     }
@@ -22,6 +24,25 @@ public class Attacking : MonoBehaviour
     }
     public void Attack()
     {
-        RaycastHit[] raycastHit = Physics.BoxCastAll(transform.position, new Vector3(), transform.forward, Quaternion.Euler(0, 0, 0), 10f, attackLayer);
+        Vector3 camPos = Camera.main.gameObject.transform.position;
+        RaycastHit[] raycastHit = Physics.BoxCastAll(transform.position, new Vector3(0.5f, 0, 0.5f), Camera.main.gameObject.transform.forward, Quaternion.identity, 5f);
+        if(raycastHit.Length > 0)
+        {
+            foreach (RaycastHit raycast in raycastHit)
+            {
+                Debug.Log("i hit " + raycast.collider.gameObject.name);
+                if (raycast.collider.name != "Capsule" && raycast.collider.name != "Sphere")
+                    target.transform.position = raycast.point;
+            }
+        }
+        //RaycastHit hit;
+        //Physics.Raycast(transform.position, Camera.main.gameObject.transform.forward, out hit, 5);
+        //if (hit.collider.name != "Capsule" && hit.collider.name != "Sphere")
+        //    target.transform.position = hit.point;
+    }
+    private void OnDrawGizmos()
+    {
+        Vector3 camPos = Camera.main.gameObject.transform.position;
+        Gizmos.DrawCube(new Vector3(camPos.x, camPos.y, camPos.z + 1), new Vector3(1, 1, 1));
     }
 }
